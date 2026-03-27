170c39b2a7b8ded336ec9db9f67b1c10d24da38a
kingmisha10 <oyelowopeter790@gmail.com>
2026-03-27 21:44:39 +0100

    Refactor login and signup event listeners

--- auth.js
+++ auth.js
@@ -4,83 +4,24 @@ const BASE_URL = "https://simple-crud-backend-6o49.onrender.com";
 const login = document.getElementById("loginForm");
 
 if (login) {
-  login.addEventListener("submit", async (e) => {
+  login.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    localStorage.setItem("user", "student");
 
-    try {
-      const res = await fetch(`${BASE_URL}/login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email,
-          password,
-        }),
-      });
-
-      const data = await res.json();
-
-      if (res.ok) {
-        // Save token/user
-        localStorage.setItem("user", JSON.stringify(data.user));
-        localStorage.setItem("token", data.token);
-
-        window.location.href = "dashboard.html";
-      } else {
-        alert(data.message || "Login failed");
-      }
-    } catch (err) {
-      alert("Server error, try again");
-    }
+    window.location.href = "dashboard.html";
   });
 }
-
 /* ================= SIGNUP ================= */
 const signup = document.getElementById("signupForm");
 
 if (signup) {
-  signup.addEventListener("submit", async (e) => {
+  signup.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const confirmPassword =
-      document.getElementById("confirmPassword").value;
+    alert("Account created successfully");
 
-    if (password !== confirmPassword) {
-      alert("Passwords do not match");
-      return;
-    }
-
-    try {
-      const res = await fetch(`${BASE_URL}/signup`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name,
-          email,
-          password,
-        }),
-      });
-
-      const data = await res.json();
-
-      if (res.ok) {
-        alert("Account created successfully");
-        window.location.href = "login.html";
-      } else {
-        alert(data.message || "Signup failed");
-      }
-    } catch (err) {
-      alert("Server error, try again");
-    }
+    window.location.href = "login.html";
   });
 }
 
